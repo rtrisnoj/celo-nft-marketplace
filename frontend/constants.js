f11668e896a86ca80034a7a288fe9db737cf1f99
Ryan Trisnojoyo <RyanTrisnojoyo@Ryans-MacBook-Pro-3.local>
2022-11-12 15:49:17 +0700

    first commit

---
+++ frontend/constants.js
@@ -0,0 +1,4 @@
+export const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/name/rtrisnoj/celo-nft-marketplace";
+
+export const MARKETPLACE_ADDRESS = "0x0d6597ad247b7aDb94453DfadE2AbEc2d3A852d9";
